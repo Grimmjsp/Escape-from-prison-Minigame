1d27ef25c8f4798a174669205ac46430d4b53a7b
Grimm <david.lemos90@gmail.com>
2022-09-22 00:30:20 +0200

    First idea

---
+++ MiniGame.py
@@ -0,0 +1,6 @@
+# Mini Game based on simple decisions
+
+from cgi import print_arguments
+
+
+print("Has sido encarcelado por robar en un banco nacional. Delante de la puerta está el guardia con las llaves de la celda y debajo de la cama hay un clip.\n Puedes usar el clip cuando el guarda se marche para tratar de abrir la celda o\n puedes crear una distracción para que el guardia entre y noquearlo para coger las llaves.")
